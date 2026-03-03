88807a24ba13274736e1a9401133dff520b2ca7d
Yuonus <syuonus@ndc.nasa.gov>
2026-03-03 13:20:52 -0500

    Adding new class

--- src/nationWideMotorsLLC/RandomClass.java
+++ src/nationWideMotorsLLC/RandomClass.java
@@ -1,6 +1,22 @@
 package nationWideMotorsLLC;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+
 public class RandomClass {
+	
+	public static void main(String[] args) {
+		
+		Integer[] nums = {1, 2, 3};
+		
+		ArrayList<Integer> num = new ArrayList<>(Arrays.asList(nums));
+		System.out.println(num.size());
+		System.out.println(num.add(123));
+		
+		
+		
+	}
 
 }
 

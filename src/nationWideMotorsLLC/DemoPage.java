433a33c8db2b5f6eac3da6ba2c2c56c2d01cb671
Yuonus <syuonus@ndc.nasa.gov>
2026-03-03 14:05:02 -0500

    The Demo file was added

---
+++ src/nationWideMotorsLLC/DemoPage.java
@@ -0,0 +1,13 @@
+package nationWideMotorsLLC;
+
+public class DemoPage {
+
+	public void m1() {
+		System.out.println("I am trying to replicate the merge conflicts.");
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
